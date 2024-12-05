1815890513dba1a5a1ba6f376d33e6d48005ee2b
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-12-05 17:11:46 +0000

    update scripte to npm run

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+app.get('/user/:id', (req, res) => {
+    res.send(`User ID: ${req.params.id}`);
+});
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
