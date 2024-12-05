349bf5ccdf5f269d2f2ca427eed3972facfccaf8
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-12-05 17:31:38 +0000

    Organized routes using Express Router

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 // index.js
 
 import express from 'express';
+import mainRoutes from './routes/index.js';
 
 const app = express();
 const port = 3000;
 
-// Basic route
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.use('/', mainRoutes);
 
-app.get('/about', (req, res) => {
-    res.send('About Us');
-});
-
-app.get('/user/:id', (req, res) => {
-    res.send(`User ID: ${req.params.id}`);
-});
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello from the main router!');
+});
+
+router.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+export default router;
